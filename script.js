914e86e4c357abfbcf1a61545b9a8e8cefa2c7b1
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-05-29 19:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
+const inputs = document.getElementsByTagName("input");
+const fName = inputs[0];
+const lName = inputs[1];
+const submitBtn = inputs[2];
+
+submitBtn.addEventListener("click", getFormvalue);
+function getFormvalue(e) {
     //Write your code here
 
+    e.preventDefault();
+    const firstName = fName.value;
+    const lastName = lName.value;
+    alert(`${firstName} ${lastName}`);
 }
